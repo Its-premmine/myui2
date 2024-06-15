6c32c868501dea54c910f7182df11186dcc63e79
Its-premmine <pprakashdumra@gmail.com>
2024-06-15 17:29:18 +0530

    responsive settiong

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ const App = () => {
                 <h1 className="myacctitle">My account</h1>
                 <div className="spandiv"><span className="signinbox"><u>Sign in</u></span><span className="creatacc">Create An Account</span></div>
                 <input type="text" className="inputField" placeholder="Username or email address " name="name"/>
-                
+                                
                 <br />
                 <input type="password" className="inputFieldpassword" placeholder="password" />
                 <br />
