57dcfc56a977c6c661b6948ff3d513df91ec18cd
Its-premmine <pprakashdumra@gmail.com>
2024-06-20 12:35:26 +0530

    second error

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import React from "react";
-import "./App.scss"
+import "./App.scss";
+
+
+
 
 const App = () => {
     return(
         <div className="mainbody">
-            <div className="contantdiv">
+            
+            <form className="contantdiv">
                 <h1 className="myacctitle">My account</h1>
                 <div className="spandiv"><span className="signinbox"><u>Sign in</u></span><span className="creatacc">Create An Account</span></div>
                 <input type="text" className="inputField" placeholder="Username or email address " name="name"/>
@@ -13,13 +17,15 @@ const App = () => {
                 <input type="password" className="inputFieldpassword" placeholder="password" />
                 <br />
                 
-                <input type="checkbox" className="checkbox"/>
-                <label for="html">Remember me</label>
+                <input type="checkbox" name="checkbox1" id="checkbox1" className="checkbox"/>
+                <label for="checkbox1">Remember me</label>
                 <br />
                 
                 <input type="submit" className="passBut"  value="SIGN IN" />
                 <br />
-            </div>
+
+            </form>
+            
 
             
             
