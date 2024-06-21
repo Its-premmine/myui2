b6c21413f689fd6954deff58c9ac99f6af893722
Its-premmine <pprakashdumra@gmail.com>
2024-06-21 23:30:58 +0530

    all perfect

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.scss";
 
 
 
 
 const App = () => {
+    const [LoginData, setLoginData] = useState({
+        Email : "",
+        password : ""
+    });
+    const handelChange = (e) => {
+        const {name, value} = e.target;
+        setLoginData((prevData) => ({
+          ...prevData,
+          [name] : value
+        }));
+      };
+      const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log("Login ID :",LoginData);
+        alert("login Successfull")
+      }
+
     return(
         <div className="mainbody">
             
-            <form className="contantdiv">
+            <form className="contantdiv" onSubmit={handleSubmit}>
                 <h1 className="myacctitle">My account</h1>
                 <div className="spandiv"><span className="signinbox"><u>Sign in</u></span><span className="creatacc">Create An Account</span></div>
-                <input type="text" className="inputField" placeholder="Username or email address " name="name"/>
+                <input type="Email" 
+                    name = "Email"
+                    value={LoginData.Email}
+                    onChange={handelChange}
+                className="inputField" placeholder="Username or email address " />
                                 
                 <br />
-                <input type="password" className="inputFieldpassword" placeholder="password" />
+                <input type="password"
+                    name="password"
+                    value={LoginData.password}
+                    onChange={handelChange}
+                className="inputFieldpassword" placeholder="password" />
                 <br />
                 
                 <input type="checkbox" name="checkbox1" id="checkbox1" className="checkbox"/>
